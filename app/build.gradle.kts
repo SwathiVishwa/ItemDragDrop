31f9d64b5edd660ff8309ca5d702725b726d0f2e
Swathi <Swathika@applaunch.io>
2024-02-08 18:49:20 +0530

    Library Added for Re Order

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -60,6 +62,19 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("org.burnoutcrew.composereorderable:reorderable:0.9.6")
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    //ComposeReOrder
+    implementation("sh.calvin.reorderable:reorderable:1.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/app/itemdragdrop/MainActivity.kt
+++ app/src/main/java/com/app/itemdragdrop/MainActivity.kt
@@ -1,28 +1,98 @@
 package com.app.itemdragdrop
 
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.app.itemdragdrop.ui.theme.ItemDragDropTheme
+import dagger.hilt.android.AndroidEntryPoint
+import sh.calvin.reorderable.ReorderableColumn
+import sh.calvin.reorderable.ReorderableItem
+import sh.calvin.reorderable.rememberReorderableLazyColumnState
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ItemDragDropTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ShowList()
+
+                SHowReorderColumn()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SHowReorderColumn() {
+    val view = LocalView.current
+
+    var list by remember { mutableStateOf(List(4) { "Item $it" }) }
+
+    ReorderableColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        list = list,
+        onSettle = { fromIndex, toIndex ->
+            list = list.toMutableList().apply {
+                add(toIndex, removeAt(fromIndex))
+            }
+        },
+        onMove = {
+            view.performHapticFeedback(HapticFeedbackConstants.SEGMENT_FREQUENT_TICK)
+        },
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) { _, item, isDragging ->
+        key(item) {
+            val elevation by animateDpAsState(if (isDragging) 4.dp else 0.dp)
+
+            Surface(shadowElevation = elevation) {
+                Row {
+                    Text(item, Modifier.padding(horizontal = 8.dp))
+                    IconButton(
+                        modifier = Modifier.draggableHandle(
+                            onDragStarted = {
+                                view.performHapticFeedback(HapticFeedbackConstants.DRAG_START)
+                            },
+                            onDragStopped = {
+                                view.performHapticFeedback(HapticFeedbackConstants.GESTURE_END)
+                            },
+                        ),
+                        onClick = {},
+                    ) {
+                        Icon(Icons.Rounded.List, contentDescription = "Reorder")
+                    }
                 }
             }
         }
@@ -30,10 +100,19 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ItemDragDropTheme {
-        ShowList()
-    }
+fun getList(): ArrayList<ListData> {
+    val list = ArrayList<ListData>()
+    list.add(
+        ListData("Row", 1)
+    )
+    list.add(ListData("Column", 2))
+    list.add(ListData("Box", 3))
+    list.add(ListData("Divider", 4))
+    list.add(ListData("Text", 5))
+    list.add(ListData("Card", 6))
+    list.add(ListData("LazyColumn", 7))
+    list.add(
+        ListData("LazyRow", 8)
+    )
+    return list
 }
